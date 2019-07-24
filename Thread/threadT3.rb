2aa8dbe8727bed8e469a1bef584822953ea7ca2d
ronald.lopes <ronald.s.lopes96@gmail.com>
2019-07-24 19:08:14 -0300

    atualizado questao T3

--- Thread/threadT3.rb
+++ Thread/threadT3.rb
@@ -3,70 +3,63 @@ require 'thread'
 mutex = Mutex.new
 resource = ConditionVariable.new
 
-
-def func(numThread, tempoDormir,mutex)
-    puts "Sou a Thread #{numThread} e vou dormir por #{tempoDormir} segundos...."
-    Thread.current.name = "Verde"
-    print(Thread.current.priority)
-    puts ""
-    sleep(tempoDormir)
-    mutex.synchronize do
-        puts("Vermelho | Thread > #{numThread}")
-        puts("Amarelo | Thread > #{numThread}")
-        puts("Verde | Thread > #{numThread}")
-    end
-end
 def printRed(tempoDormir,n,mutex,cv)
+    Thread.current.priority=1
     Thread.current.name ="Red"
-    Thread.current.priority=-3
     
     mutex.synchronize do
         n.times do
             cv.signal
-            # puts "Prioridade > #{Thread.current.priority}"
-            puts "Vermelho !!! dormindo por #{tempoDormir} segundos...."
             sleep(tempoDormir)
+            print("Vermelho !!! dormi por #{tempoDormir} segundos....\n")
+            
             cv.wait(mutex)
         end
     end
-    Thread.exit
+    
 end
 def printYellow(tempoDormir,n,mutex,cv)
-    Thread.current.name ="Yellow"
-    Thread.current.priority=-2
+    
+    Thread.current.name="Yellow"
+    Thread.current.priority=2
     mutex.synchronize do
         n.times do
             cv.signal
-            puts "Amarelo !!! dormindo por #{tempoDormir} segundos...."
-            sleep(tempoDormir) 
+            sleep(tempoDormir)
+            print "Amarelo !!! dormi por #{tempoDormir} segundos....\n"
+             
             cv.wait(mutex)          
         end
     end
-    Thread.exit
+    
 end
 def printGreen(tempoDormir,n,mutex,cv)
+    
     Thread.current.name ="Verde"
-    Thread.current.priority=-1
+    Thread.current.priority=3
     mutex.synchronize do
         n.times do
             cv.signal
-            puts "Verde !!! dormindo por #{tempoDormir} segundos...."
             sleep(tempoDormir)
+            print "Verde !!! dormi por #{tempoDormir} segundos....\n"
+            
             cv.wait(mutex)
         end
     end
-    Thread.exit
+   
 end
 puts "Informe o numero de repetições: "
 repeticao = gets.to_i
 
 thread = []
-thread << Thread.new{printRed(Random.rand(1..10),repeticao,mutex,resource)}
-thread << Thread.new{ printYellow(Random.rand(1..10),repeticao,mutex,resource)}
-thread << Thread.new{ printGreen(Random.rand(1..10),repeticao,mutex,resource)}
-
-thread.each do |t|
-    
-    t.join
-    
+mutex.synchronize do
+    thread << Thread.new{ Thread.stop;printRed(Random.rand(1..10),repeticao,mutex,resource)}
+    thread << Thread.new{  Thread.stop;printYellow(Random.rand(1..10),repeticao,mutex,resource)}
+    thread << Thread.new{  Thread.stop;printGreen(Random.rand(1..10),repeticao,mutex,resource)}
 end
+
+sleep(1)
+thread.each do |t|
+    t.run
+end
+thread[0].join
